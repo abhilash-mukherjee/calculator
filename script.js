92895b65029409b5cf038b92d07ba349b66b5dc8
Abhilash Mukherjee <abhilash301267@gmail.com>
2022-08-23 14:11:29 +0530

    all buttons working. Keyboard compatible

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const MAX_DIGITS = 5;
-const MAX_DECIMALS = 3;
+const MAX_DIGITS = 6;
+const MAX_DECIMALS = 4;
 const LIMIT_MESSEGE = "LIMIT EXCEEDED!!!";
 const DIVIDED_BY_ZERO_MESSEGE = "CAN'T DIVIDE BY ZERO!!!";
 const display = document.querySelector('.display');
@@ -11,11 +11,12 @@ const clearBtn = document.querySelector('#clear');
 const allBtns = document.querySelectorAll('button');
 const decimalBtn = document.querySelector('#decimal');
 const backSpace = document.querySelector("#back-space");
+const changeSignBtn = document.querySelector("#change-sign");
 const operatorMap = {
-    'sum': { func: sum, symbol: '+'},
-    'difference': { func: difference, symbol: '-'},
-    'product': { func: product, symbol: '*'},
-    'divide': { func: divide, symbol: '/'},
+    '+': { func: sum, symbol: '+'},
+    '-': { func: difference, symbol: '-'},
+    '*': { func: product, symbol: '*'},
+    '/': { func: divide, symbol: '/'},
 }
 
 const calculation = {
@@ -26,21 +27,65 @@ const calculation = {
     number2:{
         value: NaN,
     },
-    operator: undefined,
+    operator: 
+    {
+        func: undefined,
+        symbol: '',
+    },
+    eavluationDone: false,
     status:'',
     getResult: function(){
-        let result = operate(this.operator,this.number1.value,this.number2.value);
+        let result = operate(this.operator.func,this.number1.value,this.number2.value);
         console.log(result);
         return parseFloat(result.toFixed(3))
     },
-    getOperatorSymbol: function(){
-        if(!this.operator) return undefined;
-        return operatorMap[this.operator.name]['symbol'];
-    }
 }
 
 //************************Business Logic*********************************/
 
+window.addEventListener('keydown',(event) =>
+ {
+    if(!isNaN(parseInt(event.key)) ) {
+        console.log('cool');
+        addNumberEvents(parseInt(event.key));
+        updateCalcStatus();
+        return;
+    }
+    if(event.key === '.'){
+        addDecimal();
+        updateCalcStatus();
+        return;
+    }
+    if(
+        event.key === '+' || 
+        event.key === '-' ||
+        event.key === '*' || 
+        event.key === '/'
+    ){
+        addOperatorEvents(event.key);
+        updateCalcStatus();
+        return;
+    }
+    if(
+        event.key === '=' || 
+        event.key === 'Enter'
+    ){
+        executeCalculation();
+        updateCalcStatus();
+        return;
+    }
+    if(event.key === 'Backspace'){
+        deleteNumber();
+        updateCalcStatus();
+        return;
+    }
+    if(event.key === 'c'){
+        clearCache();
+        updateCalcStatus();
+        return;
+    }
+
+})
 equalsBtn.addEventListener('click', executeCalculation);
 
 clearBtn.addEventListener('click',clearCache);
@@ -48,27 +93,28 @@ clearBtn.addEventListener('click',clearCache);
 for(let numberBtn of numberBtns)
 {
     console.log(numberBtn);
-
-    numberBtn.addEventListener('click',addNumberEvents);
+    numberBtn.addEventListener('click',(e) => {addNumberEvents(e.target.textContent)});
 }
 
 for(let operatorBtn of operatorBtns)
 {
-    operatorBtn.addEventListener('click',addOperatorEvents);
-    
+    operatorBtn.addEventListener('click',
+    (e) => {
+        addOperatorEvents(e.target.textContent);
+    }
+    );   
 }
 backSpace.addEventListener('click',deleteNumber);
 decimalBtn.addEventListener('click',addDecimal);
+changeSignBtn.addEventListener('click',changeSign)
 allBtns.forEach(()=>{addEventListener('click',updateCalcStatus)});
-
-function addOperatorEvents(e)
+function addOperatorEvents(symbol)
 {
     console.log("operator pressed");
     if(isNaN(calculation.number1.value))return;
     if(!isNaN(calculation.number1.value))
     {
         calculation.number1.isFinalized = true;
-        clearDisplay();
     }
     if(!isNaN(calculation.number2.value))
     {
@@ -76,19 +122,32 @@ function addOperatorEvents(e)
         calculation.number2.value = NaN;
         displayNumber(`${calculation.number1.value}`);
     }
-    calculation.operator = operatorMap[e.target.getAttribute('id')].func;
+    clearDisplay();
+    calculation.operator.func = operatorMap[symbol].func;
+    calculation.operator.symbol = symbol;
+    calculation.eavluationDone = false;
 }
 
-function addNumberEvents()
+function addNumberEvents(number)
 {
     if(display.textContent.localeCompare('NaN') === 0) 
     clearCache();
     if(calculationStatus.textContent.localeCompare(LIMIT_MESSEGE) === 0) 
     clearCache();
-
-    if(calculation.number1.isFinalized && isNaN(calculation.number2.value))clearDisplay();
+    if(calculation.eavluationDone)
+    {
+        clearCache();
+        calculation.eavluationDone = false;
+    }
     
-    addNumberToDisplay(this.textContent); 
+    if(
+        calculation.number1.isFinalized &&
+        isNaN(calculation.number2.value) &&
+        !display.textContent.includes('-')
+    )
+    clearDisplay();
+    
+    addToDisplay(number); 
     updateNumbers();
 }
 
@@ -108,19 +167,23 @@ function executeCalculation()
     if
     (
         isNaN(calculation.number1.value) || 
-        !calculation.operator || 
+        !calculation.operator.func || 
         isNaN(calculation.number2.value)
     )
     {
-        calculation.operator = undefined;
+        calculation.operator.func = undefined;
+        calculation.operator.symbol = '';
         calculation.number2.value = NaN;
+        calculation.number1.isFinalized = false;
         return;
     }
 
     calculation.number1.value = calculation.getResult();
     calculation.number1.isFinalized = false;
     calculation.number2.value = NaN;
-    calculation.operator = undefined;
+    calculation.operator.func = undefined;
+    calculation.operator.symbol = '';
+    calculation.eavluationDone = true;
     displayNumber(calculation.number1.value);
 }
 
@@ -130,14 +193,15 @@ function clearCache()
     calculation.number1.value = NaN;
     calculation.number1.isFinalized = false;
     calculation.number2.value = NaN;
-    calculation.operator = undefined;
+    calculation.operator.func = undefined;
+    calculation.operator.symbol = '';
     calculation.status = '';
 }
 
 function deleteNumber()
 {
     if(display.textContent.length === 0) return;
-    if(calculation.operator && isNaN(calculation.number2.value)) return;
+    if(calculation.operator.func && isNaN(calculation.number2.value)) return;
     display.textContent = display.textContent.slice(0,-1);
     updateNumbers();
     updateCalcStatus();
@@ -145,10 +209,38 @@ function deleteNumber()
 
 function addDecimal()
 {
+    if(calculation.eavluationDone)
+    {
+        clearCache();
+        calculation.eavluationDone = false;
+    }
     if(display.textContent.includes('.'))return;
-    display.textContent = `${display.textContent}.`
+    if( isNaN(calculation.number1.value))
+    {
+        if(display.textContent.includes('-'))
+        display.textContent = `-0.`;
+        else display.textContent = `0.`;
+    }
+    else if(calculation.number1.isFinalized && isNaN(calculation.number2.value))
+    {
+        display.textContent = `0.`;
+    }
+    else display.textContent = `${display.textContent}.`;
+    updateNumbers();
     updateCalcStatus();
 }
+
+function changeSign()
+{
+    if(display.textContent.includes('-'))
+    display.textContent = display.textContent.slice(1);
+    else
+    display.textContent = `-${display.textContent}`;
+    updateNumbers();
+    updateCalcStatus();
+}
+
+
 //************************Business Logic*********************************/
 
 /**********************************************************************/
@@ -170,7 +262,7 @@ function clearCalcStatusDisplay()
 function displayNumber(number)
 {
     console.log('inside Display Number.' + number);
-    if(number.toString().length > (MAX_DIGITS + MAX_DECIMALS ))
+    if(hasExceededLimit(number.toString()))
     {
         console.log("Limit Exceeded");
         clearCache();
@@ -179,6 +271,27 @@ function displayNumber(number)
     }
     if(isNaN(number)) calculation.status = DIVIDED_BY_ZERO_MESSEGE;
     display.textContent = number;
+}
+
+function hasExceededLimit(string)
+{
+    if(string.includes('-')) 
+    {
+        if(string.includes('.'))
+        return string.length > MAX_DIGITS + MAX_DECIMALS + 2;
+        else
+        return string.length > MAX_DIGITS + MAX_DECIMALS + 1;
+        
+    }
+
+    if(string.includes('.')) 
+    {    
+        return string.length > MAX_DIGITS + MAX_DECIMALS + 1;
+    }
+    else
+    {
+        return string.length > MAX_DIGITS + MAX_DECIMALS;    
+    }
 }
 
 function updateCalcStatus()
@@ -192,24 +305,32 @@ function updateCalcStatus()
     let str = '';
     if(isNaN(calculation.number1.value))
         str = '';
-    else if (!calculation.operator)
+    else if (!calculation.operator.func)
         str = `${calculation.number1.value}`;
     else if (isNaN(calculation.number2.value))
-        str = `${calculation.number1.value} ${calculation.getOperatorSymbol()}`;
+        str = `${calculation.number1.value} ${calculation.operator.symbol}`;
     else
-        str = `${calculation.number1.value} ${calculation.getOperatorSymbol()} ${calculation.number2.value}`;
+        str = `${calculation.number1.value} ${calculation.operator.symbol} ${calculation.number2.value}`;
     calculationStatus.textContent = str;
 }
 
-function addNumberToDisplay(digit)
+function addToDisplay(digit)
 {
     let str = display.textContent;
     let splitArr = str.split('.');
-    if(
-        splitArr[0]?.length > MAX_DIGITS-1 
-        ||
-        splitArr[1]?.length > MAX_DECIMALS-1
-    ) return;
+    if(str.includes('.')) 
+    {
+        if(splitArr[1]?.length > MAX_DECIMALS-1 )
+        {
+            console.log('decimal lt exceeded');
+            return;
+        }
+    }
+    else if(splitArr[0]?.length > MAX_DIGITS-1)
+    {
+        console.log('digit limit exceeded');
+        return;
+    }
     displayNumber(`${display.textContent}${digit}`);
 }
 
